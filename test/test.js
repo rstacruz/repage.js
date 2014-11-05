688d646b5b1b61863408f66e62536fb3feee4aee
Rico Sta. Cruz <hi@ricostacruz.com>
2014-11-05 10:42:18 +0800

    Test page properly

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var repage, page, expect;
+var page, expect;
 var isNode = typeof process === 'object';
 
 if (isNode) {
@@ -9,69 +9,68 @@ if (isNode) {
 }
 
 before(function () {
-  repage = require('../index');
-  page = require('page');
+  page = require('../index');
 });
 
 before(function () {
   if (isNode)
-    repage({ popstate: false });
+    page({ popstate: false });
   else
-    repage();
+    page();
 });
 
 describe('ctx.querystring', function () {
   it('defaults to ""', function (done) {
-    repage('/querystring-default', function (ctx) {
+    page('/querystring-default', function (ctx) {
       expect(ctx.querystring).to.equal('');
       done();
     });
 
-    repage('/querystring-default');
+    page('/querystring-default');
   });
 });
 
 describe('.show()', function () {
   it('works', function (done) {
-    repage('/showid/:id', function (ctx) {
+    page('/showid/:id', function (ctx) {
       expect(ctx.params.id).eql('john');
       done();
     });
 
-    repage('/showid/:id', { id: 'john' });
+    page('/showid/:id', { id: 'john' });
   });
 
   it('handles page(uri)', function (done) {
-    repage('/show/hello', function (ctx) { done(); });
-    repage('/show/hello');
+    page('/show/hello', function (ctx) { done(); });
+    page('/show/hello');
   });
 
   it('handles page(uri, {})', function (done) {
-    repage('/show/user/:id', function (ctx) {
+    page('/show/user/:id', function (ctx) {
       expect(ctx.params.id).eq('2');
       done();
     });
-    repage('/show/user/:id', { id: 2 });
+    page('/show/user/:id', { id: 2 });
   });
 });
 
 describe('.uri()', function(){
   it('should work for uri(string)', function() {
-    var str = repage.uri('/users');
+    var str = page.uri('/users');
     expect(str).to.eq('/users');
   });
 
   it('should work for uri(string, params)', function() {
-    var str = repage.uri('/user/:id', { id: 2 });    expect(str).to.eq('/user/2');
+    var str = page.uri('/user/:id', { id: 2 });    expect(str).to.eq('/user/2');
   });
 
   it('should work with query strings', function() {
-    var str = repage.uri('/search', { q: 'hello' });
+    var str = page.uri('/search', { q: 'hello' });
     expect(str).to.eq('/search?q=hello');
   });
 
   it('should work with both params and query strings', function() {
-    var str = repage.uri('/:country/search', { country: 'de', q: 'hello' });
+    var str = page.uri('/:country/search', { country: 'de', q: 'hello' });
     expect(str).to.eq('/de/search?q=hello');
   });
 });
@@ -79,32 +78,32 @@ describe('.uri()', function(){
 
 describe('.querystring()', function(){
   it('should work with a single key-value', function() {
-    var str = repage.querystring({ q: 'hello world' });
+    var str = page.querystring({ q: 'hello world' });
     expect(str).to.eq('q=hello%20world');
   });
 
   it('should handle arrays', function() {
-    var str = repage.querystring({ q: 'hello world', attrs: ['title','author'] });
+    var str = page.querystring({ q: 'hello world', attrs: ['title','author'] });
     expect(str).to.eq('q=hello%20world&attrs[]=title&attrs[]=author');
   });
 
   it('should discard undefineds', function() {
-    var str = repage.querystring({ q: 'hello world', context: undefined });
+    var str = page.querystring({ q: 'hello world', context: undefined });
     expect(str).to.eq('q=hello%20world');
   });
 
   it('should include nulls', function() {
-    var str = repage.querystring({ q: 'hello world', context: null });
+    var str = page.querystring({ q: 'hello world', context: null });
     expect(str).to.eq('q=hello%20world&context=');
   });
 
   it('should handle objects', function() {
-    var str = repage.querystring({ book: {title: 'hello', author: 'world'} });
+    var str = page.querystring({ book: {title: 'hello', author: 'world'} });
     expect(str).to.eq('book[title]=hello&book[author]=world');
   });
 
   it('should handle recursive objects', function() {
-    var str = repage.querystring({ my: { book: {title: 'hello', author: 'world'} }});
+    var str = page.querystring({ my: { book: {title: 'hello', author: 'world'} }});
     expect(str).to.eq('my[book][title]=hello&my[book][author]=world');
   });
 });
@@ -113,44 +112,44 @@ describe('.replace()', function() {
   it('should not change history.length', function(done) {
     var length = history.length;
 
-    repage('/replace/x', function(ctx){
+    page('/replace/x', function(ctx){
       expect(history.length).to.eq(length);
       done();
     });
-    repage.replace('/replace/x');
+    page.replace('/replace/x');
   });
 });
 
 describe('.replace(uri)', function() {
   it('should work with replace(uri)', function(done) {
-    repage('/replace/hello', function(ctx){ done(); });
-    repage.replace('/replace/hello');
+    page('/replace/hello', function(ctx){ done(); });
+    page.replace('/replace/hello');
   });
 
   it('should work with replace(uri, {})', function(done) {
-    repage('/replace/user/:id', function(ctx){
+    page('/replace/user/:id', function(ctx){
       expect(ctx.params.id).eq('2');
       done();
     });
-    repage.replace('/replace/user/:id', { id: 2 });
+    page.replace('/replace/user/:id', { id: 2 });
   });
 });
 
 
 describe('.back()', function () {
   it('should go back', function () {
-    repage('/back/1', function () {});
-    repage('/back/2', function () {});
+    page('/back/1', function () {});
+    page('/back/2', function () {});
 
-    repage('/back/1');
-    repage('/back/2');
-    repage.back();
+    page('/back/1');
+    page('/back/2');
+    page.back();
     expect(location.pathname).to.eq('/back/1');
   });
 });
 
 describe('.stop()', function() {
   it('doesn\'t produce errors', function() {
-    repage.stop();
+    page.stop();
   });
 });
