e1b84601192ac130779132e05b1091c0959b94bc
Rico Sta. Cruz <hi@ricostacruz.com>
2014-11-05 10:59:03 +0800

    Big documentation update

--- index.js
+++ index.js
@@ -10,6 +10,39 @@
 
 }(this, function (page) {
 
+  /*
+   * Setimmediate helper
+   */
+
+  var setImmediate = this.setImmediate ?
+    this.setImmediate :
+    function (fn) { window.setTimeout(fn, 0); };
+
+  /**
+   * page([options]):
+   * Starts page.js.
+   *
+   * Register page's `popstate` / `click` bindings. If you're doing selective
+   * binding you'll like want to pass `{ click: false }` to specify this
+   * yourself. The following options are available:
+   *
+   * - `click` bind to click events [__true__]
+   * - `popstate` bind to popstate [__true__]
+   * - `dispatch` perform initial dispatch [true]
+   *
+   * If you wish to load serve initial content from the server you likely will
+   * want to set `dispatch` to __false__.
+   *
+   *     var page = require('repage');
+   *     page('/', index);
+   *     page('/user/:user', show);
+   *     page('/user/:user/edit', edit);
+   *     page('/user/:user/album', album);
+   *     page('/user/:user/album/sort', sort);
+   *     page('*', notfound);
+   *     page();
+   */
+
   function repage (path, fn) {
     // page(function)
     if ('function' == typeof path) {
@@ -29,20 +62,71 @@
     }
   }
 
-  repage.start = page.start;
-  repage.stop = page.stop;
-
   /**
-   * len : repage.len
-   * Number of pages navigated to.
+   * Quick reference:
+   *
+   *     // routing:
+   *     page('/', index)           // calls `index()` when navigating to /
+   *     page('*', notfound)        // calls `notfound()` for all pages
+   *     page.base('/blog')         // sets the base path
+   *     
+   *     // navigation:
+   *     page('/users')             // navigate to /users
+   *     page.replace('/users')     // replaces the current state with /users
+   *
+   * Only in repage.js:
+   *
+   *     page('/user/:id', { id: 20 })           // navigates to /user/20
+   *     page('/search', { q: 'hello' })         // navigates to /search?q=hello
+   *     page.replace('/search', { q: 'hello' }) // navigates to /user/20 by replacing
+   *     
+   *     page.uri('/user/:id', { id: 20 })       // returns "/user/20" (string)
+   *     page.redirect('/users')                 // redirects to /users from a route
+   *     
+   *     page.back()                             // goes back, or returns home if available
    */
 
-  repage.len = 0;
+  /*
+   * Propagate unchanged things
+   */
+
+  repage.start = page.start;
+  repage.stop = page.stop;
+  repage.dispatch = page.dispatch;
+
+  /**
+   * page(path) : page(path, [params])
+   * Navigate to the given `path`.
+   *
+   *     $('.view').click(function (e) {
+   *       e.preventDefault();
+   *       page('/user/12');
+   *     });
+   *
+   * You may also specify `params` for params to be replaced in the `path`s 
+   * placeholders.
+   *
+   *     page('/user/:id', { id: 12 });
+   *     // same as `page('/user/12')`
+   */
 
   repage.show = function (path, params, dispatch) {
     var uri = repage.uri(path, params);
     page.show(uri, {}, dispatch);
   };
+
+  /**
+   * page.replace():
+   * Works like `page(path)`, but replaces the current state instead of pushing
+   * it. Great for form submission pages.
+   *
+   *     $('.submit').on('click', function () {
+   *       $.post('/submit', function (article) {
+   *         alert("data saved");
+   *         page.replace('/article/:id', { id: article.id });
+   *       });
+   *     });
+   */
 
   repage.replace = function (path, params, init, dispatch) {
     var uri = repage.uri(path, params);
@@ -50,7 +134,18 @@
   };
 
   /**
-   * uri() : uri(path, options)
+   * len : repage.len
+   * Number of pages navigated to.
+   *
+   *     page.len == 0;
+   *     page('/login');
+   *     page.len == 1;
+   */
+
+  repage.len = 0;
+
+  /**
+   * page.uri() : uri(path, options)
    * Builds a URI path with dynamic parameters, mimicking Express's conventions.
    *
    *     page.uri('/api/users/:id', { id: 24 });
@@ -79,7 +174,7 @@
   };
 
   /**
-   * querystring() : querystring(data)
+   * querystring() : page.querystring(data)
    * Converts an object into a query string.
    *
    *     page.querystring({ name: 'john smith', count: 3 })
@@ -117,11 +212,16 @@
     return pairs.join('&');
   };
 
-
   /**
-   * back() : back([path])
+   * back() : page.back([path])
    * Goes back. If `path` is given, it will navigate to that instead when
    * there's no page to go back to.
+   *
+   *     document.getElementById('back').onclick = function() {
+   *       // either goes back, or returns to the homepage when there's
+   *       // no page to go back to.
+   *       page.back('/');
+   *     };
    */
 
   repage.back = function(path, params) {
@@ -130,6 +230,27 @@
     } else if (arguments.length > 0) {
       repage(path, params);
     }
+  };
+
+  /**
+   * page.redirect() : page.redirect(path, params)
+   * Navigates to `path`. Works like `page.show()` or `page.replace()`, but
+   * suitable to be used inside a route.
+   *
+   *     page('/login', function (ctx) {
+   *       page.redirect('/sessions/new');
+   *     });
+   *
+   *     page('/dashboard', function (ctx) {
+   *       if (!authenticated)
+   *         page.redirect('/login');
+   *     });
+   */
+
+  page.redirect = function(path, params) {
+    setImmediate(function (){
+      page.replace(path, params);
+    });
   };
 
   /*
